e6d6701c82182510aa96fa7131af92f349627655
MrTango <md@derico.de>
2019-04-26 16:16:26 +0300

    "Add view: consent-item-consents-view"

---
+++ src/collective/consent/tests/test_view_consent_item_consents_view.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from collective.consent.testing import COLLECTIVE_CONSENT_FUNCTIONAL_TESTING
+from collective.consent.testing import COLLECTIVE_CONSENT_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_CONSENT_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_consent_item_consents_view_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='consent-item-consents-view'
+        )
+        self.assertTrue(view.__name__ == 'consent-item-consents-view')
+        # self.assertTrue(
+        #     'Sample View' in view(),
+        #     'Sample View is not found in consent-item-consents-view'
+        # )
+
+    def test_consent_item_consents_view_not_matching_interface(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='consent-item-consents-view'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = COLLECTIVE_CONSENT_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
---
+++ src/collective/consent/views/consent_item_consents_view.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from collective.consent import _
+from Products.Five.browser import BrowserView
+
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class ConsentItemConsentsView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('consent_item_consents_view.pt')
+
+    def __call__(self):
+        self.msg = _(u'A small message')
+        return self.index()
